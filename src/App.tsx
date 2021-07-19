3b7eca64ec85b889b28f1b82ab92e3851cae7d7e
Kevin Ruiz Molina <kevinrumolina26@gmail.com>
2021-07-18 23:52:30 -0500

    implement app

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState, useEffect }  from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Film } from './components/Film';
+import { Detail } from './components/Detail';
+import { BackButton } from './components/BackButton';
+import { SimilarFilm } from './components/SimilarFilm';
 import './App.css';
 
+const sectionNames = ["tv shows", "movies"];
+const sectionUrl = ["tv", "movie"];
+const apiUrl = 'https://api.themoviedb.org/3/';
+const apiKey = 'a562a9a87fddcafca2a9c4dd28aaea99';
+
+const sectionConverter = (section: string) => sectionUrl[sectionNames.indexOf(section)]
+
 function App() {
+  const [section, setSection] = useState('tv shows');
+
+  const handleChange = (event: any) => {
+    const updatedValue = event.target.getAttribute('value');
+
+    setSection(updatedValue);
+  }
+
+  const [films, setFilms] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}${sectionConverter(section)}/popular?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response.results)
+      setFilms(response.results)
+    })
+  }, [section])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/tv/">
+            <BackButton />
+            <Detail />
+            <SimilarFilm />
+          </Route>
+          <Route path="/movie/">
+            <BackButton />
+            <Detail />
+            <SimilarFilm />
+          </Route>
+          <Route path="/">
+          <header>
+            {sectionNames.map(name => (
+            <div className='menu-section' key={name}>
+              <input type="radio" className="menu-section__input" name="section" value={name} id={name}
+                onChange={handleChange} />
+              <label htmlFor={name} className="menu-section__button">
+                {name}
+              </label>
+            </div>
+            ))}
+            <h2 className="menu-section__title">{section}</h2>
+          </header>
+          <main>
+            <section className="section">
+              {films.length > 0 && films.map((film) =>
+              <Film key={film.id} {...film} />)}
+            </section>
+          </main>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
